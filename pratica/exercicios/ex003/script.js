ff73447912562675b62a85564ef3e3eb0dd2f969
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-05-17 18:12:02 -0300

    Create script.js

---
+++ pratica/exercicios/ex003/script.js
@@ -0,0 +1,9 @@
+var n1 = Number(window.prompt('Um numero... '))
+var n2 = Number(window.prompt('Outro numero... '))
+var s = n1 + n2
+
+window.alert(
+`Voce digitou ${n1} e ${n2}... 
+
+Sua Soma e ${s}.`
+)
