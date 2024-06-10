f0619b14e9499d4f34eae0a195edaa65d9d62305
Leandro Galvão <leandrogalvao.uwu@gmail.com>
2024-06-10 19:34:30 -0300

    Delete script.js

--- pratica/exercicios/ex003/script.js
+++
@@ -1,9 +0,0 @@
-var n1 = Number(window.prompt('Um numero... '))
-var n2 = Number(window.prompt('Outro numero... '))
-var s = n1 + n2
-
-window.alert(
-`Voce digitou ${n1} e ${n2}... 
-
-Sua Soma e ${s}.`
-)
